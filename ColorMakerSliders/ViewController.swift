b09512ce10e0e8ee2833d1cbe7438e9c5e7d7fd6
Benjamin-Orlang <borlang@gmail.com>
2018-05-16 11:55:49 -0400

    ColorMakerSlider Commit 1

--- ColorMakerSliders/ViewController.swift
+++ ColorMakerSliders/ViewController.swift
@@ -2,24 +2,39 @@
 //  ViewController.swift
 //  ColorMakerSliders
 //
-//  Created by alex perez on 3/9/18.
+//  Created by Ben Orlang on 3/9/18.
 //  Copyright © 2018 B.Sazon Udacity. All rights reserved.
 //
 
 import UIKit
 
+// MARK: - ViewController: UIViewController
+
 class ViewController: UIViewController {
 
+    // Add Outlets
+
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+    
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Actions
+    
+    @IBAction func changeColorComponent(_ sender: AnyObject) {
+        
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
-
-
 }
 
